06e8b1cbee0db9248ed38e68f461b1d02bf7340c
Sean Harrell <40783550+seanphenomenon@users.noreply.github.com>
2020-04-03 01:55:25 -0700

    select and where query complete

--- sql/exercises.sql
+++ sql/exercises.sql
@@ -16,6 +16,7 @@
 select * from actor;
 
 # 1b. Select only the last_name column from the actor table.
+SELECT last_name FROM actor;
 
 
 # 1c. Select only the following columns from the film table.
@@ -26,7 +27,13 @@ select * from actor;
 # rental_duration       Exists in film table.
 # rental_rate           Exists in film table.
 # total_rental_cost     rental_duration * rental_rate
-
+SELECT
+    title, 
+    description, 
+    rental_duration, 
+    rental_rate, 
+    rental_duration * rental_rate as total_rental_cost
+FROM film;
 
 # ---------------------------------------------------------#
 
@@ -34,14 +41,17 @@ select * from actor;
 ## 2. DISTINCT operator
 
 # 2a. Select all distinct (different) last names from the actor table.
-
+ 
+    SELECT DISTINCT last_name FROM actor; 
 
 # 2b. Select all distinct (different) postal codes from the address table.
+
+    SELECT DISTINCT postal_code FROM address;
 
 
 # 2c. Select all distinct (different) ratings from the film table.
 
-
+    SELECT DISTINCT rating FROM film;
 
 # ---------------------------------------------------------#
 
@@ -49,28 +59,37 @@ select * from actor;
 ## 3. WHERE clause
 
 # 3a. Select the title, description, rating, movie length columns from the films table that last 3 hours or longer.
-
+ 
+    SELECT title, description, rating, length FROM film WHERE length >= 180; 
 
 # 3b. Select the payment id, amount, and payment date columns from the payments table for payments made on or after 05/27/2005.
 
+    SELECT payment_id, amount, payment_date FROM payment WHERE payment_date >= 05/27/2005;
+
 
 # 3c. Select the primary key, amount, and payment date columns from the payment table for payments made on 05/27/2005.
-
+    
+    SELECT payment_id, amount, payment_date FROM payment WHERE DATE(payment_date)='2005-05-27';
 
 # 3d. Select all columns from the customer table for rows that have a last name beginning with "S" and a first name ending with "N".
-
+    
+    SELECT * FROM customer WHERE last_name LIKE "S%" AND first_name LIKE "%N";
 
 # 3e. Select all columns from the customer table for rows where the customer is inactive or has a last name ending with "M".
-
+    
+    SELECT * FROM customer WHERE active=0 OR last_name LIKE '%M';     
 
 # 3f. Select all columns from the category table for rows where the primary key is greater than 4 and the name field begins with either "C", "S" or "T".
-
+   
+    SELECT * FROM category WHERE category_id > 4 AND name LIKE 'C%' OR name LIKE 'S%' OR name LIKE 'T%';
 
 # 3g. Select all columns minus the password column from the staff table for rows that contain a password.
-
+    
+    SELECT * FROM staff WHERE password IS NOT NULL;
 
 # 3h. Select all columns minus the password column from the staff table for rows that do not contain a password.
 
+    SELECT * FROM staff WHERE password IS NULL;
 
 # ---------------------------------------------------------#
 
